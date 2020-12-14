bfafb8fdf494a9379554e263e7684d7f9273e491
ecnw <ericnana@mail.com>
2020-12-14 23:20:56 +0100

    Changes made to the script and added steps on how to reproduce them

--- aiclassifier.py
+++ aiclassifier.py
@@ -3,13 +3,10 @@
 
 
 import unittest
+
 from appium import webdriver
 from appium.webdriver.common.mobileby import MobileBy
-#import org.openqa.selenium.By; 
-#import org.openqa.selenium.WebElement;
-#from selenium.webdriver.remote.webdriver import WebDriver
-import time
-
+#import time
 
 
 
@@ -20,30 +17,41 @@ class TestAI_ClassifierDemoTest(unittest.TestCase):
     def setUp(self):
        desired_caps = {}
        desired_caps['platformName'] = 'Android'
-       desired_caps['deviceName'] = 'Android Emulator'#enter adb device
+       desired_caps['deviceName'] = 'Android Emulator'
        desired_caps['uiautomationName'] = 'UiAutomator2'
-       desired_caps['appPackage'] = 'com.ebay.mobile'
-       desired_caps['appActivity'] = 'com.ebay.mobile.activities.MainActivity'
-       desired_caps['customFindModules'] = {'ai':'test-ai-classifier'}
-       desired_caps['shouldUseCpactResponse'] = False
-        
-        
-       self.driver = webdriver.Remote('http://0.0.0.0:4723/wd/hub', desired_caps)
-        
        
+       #desired_caps['appPackage'] = 'com.ebay.mobile'
+       #desired_caps['appActivity'] = 'com.ebay.mobile.activities.MainActivity'
+       
+       #desired_caps['appPackage'] = 'com.walmart.mg'
+       #desired_caps['appActivity'] = 'com.mx.walmart.walmartgroceries.MainActivity'
+       
+       desired_caps['appPackage'] = 'com.android.settings'
+       desired_caps['appActivity'] = 'com.android.settings.Settings'
+       
+       desired_caps['skipServerInstallation'] = True
+       desired_caps['customFindModules'] = {'ai':'test-ai-classifier'}
+       desired_caps['shouldUseCompactResponses'] = False
+        
+       self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+       
+       #self.driver.implicitly_wait(5)
+
         
     def tearDown(self):
         self.driver.quit()
         
     
     def test_ai(self):
-        time.sleep(20)
-        #mobileby = MobileBy.CUSTOM
-        #search = self.driver.find_element_by_custom("ai:search")
-        search = self.driver.find_element_by_custom('ai:cart')
-
+        self.driver.implicitly_wait(20)
+        #time.sleep(20)
+        
+        #search = self.driver.find_element_by_custom('ai:search')
+        search = self.driver.find_element(MobileBy.CUSTOM, 'ai:search')
         search.click()
-        time.sleep(10)
+        self.driver.implicitly_wait(20)
+
+        #time.sleep(10)
 
 if __name__== '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestAI_ClassifierDemoTest)
